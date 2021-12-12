5b171e92fb7ba92c02458d99c707f10462a5ba22
KrzysztofDabal <73111562+KrzysztofDabal@users.noreply.github.com>
2021-12-12 16:31:50 +0100

    Basic api for product and storage

--- storage/routes/api.php
+++ storage/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\StorageController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +16,30 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
+
+    Route::get('/storage', [StorageController::class, 'index']);
+    Route::get('/storage/{id}', [StorageController::class, 'show']);
+    Route::get('/storage/{userid}', [StorageController::class, 'search']);
+    Route::post('/storage', [StorageController::class, 'store']);
+    Route::put('/storage/{id}', [StorageController::class, 'update']);
+    Route::delete('/storage/{id}', [StorageController::class, 'destroy']);
+
+    Route::get('/user', [AuthController::class, 'index']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
