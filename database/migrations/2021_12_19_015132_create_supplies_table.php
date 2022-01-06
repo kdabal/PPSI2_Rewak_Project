3fc08a5fdfa917230188347b4f4b58f357f89c97
kdabal <92679625+kdabal@users.noreply.github.com>
2022-01-06 10:08:08 +0100

    desperacja

---
+++ database/migrations/2021_12_19_015132_create_supplies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSuppliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('supplies', function (Blueprint $table) {
+            $table->id();
+            $table->integer('userid');
+            $table->integer('productid');
+            $table->integer('count')->nullable();
+            $table->date('date_sl')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('supplies');
+    }
+}
