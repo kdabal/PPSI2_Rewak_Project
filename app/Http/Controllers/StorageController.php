9074cd4b3422e65ec7e6983e7a6ac182b3bdecd7
kdabal <92679625+kdabal@users.noreply.github.com>
2022-01-06 09:22:56 +0100

    project initiation

---
+++ app/Http/Controllers/StorageController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Storage;
+use Illuminate\Http\Request;
+
+class StorageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Storage::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return Storage::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Storage::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $storage = Storage::find($id);
+        $storage->update($request->all());
+        return $storage;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Storage::destroy($id);
+    }
+
+    public function search($userid)
+    {
+        return Storage::where('userid', $userid)->get();
+    }
+}
