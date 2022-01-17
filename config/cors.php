aa9f740e7f899776e9ac9c01507b388115c118ea
kdabal <92679625+kdabal@users.noreply.github.com>
2022-01-17 16:32:35 +0100

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -15,9 +15,9 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*', 'api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE'],
 
     'allowed_origins' => ['*'],
 
@@ -32,3 +32,4 @@ return [
     'supports_credentials' => false,
 
 ];
+
